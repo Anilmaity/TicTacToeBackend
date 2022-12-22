cd3c5eca869489285ab71ae266ab177c76adbf4d
Anil <anilmaity18@gmail.com>
2022-12-22 18:23:03 +0530

    bug fix

--- TicTacToeBackend/settings.py
+++ TicTacToeBackend/settings.py
@@ -60,12 +60,11 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 MIDDLEWARE = [
 
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
